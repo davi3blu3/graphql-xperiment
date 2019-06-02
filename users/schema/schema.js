4f9615345753b085f61b22f02332870b07ce2622
Dave Harned <dharned@gmail.com>
2019-06-01 22:20:16 -0500

    switch user data to json-server

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
-
+const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -13,12 +12,6 @@ const UserType = new GraphQLObjectType({
   }
 });
 
-const users = [
-  { id: '23', firstName: 'Bill', lastName: 'Rogers', age: 20 },
-  { id: '47', firstName: 'Samantha', lastName: 'Green', age: 22 },
-  { id: '51', firstName: 'Susan', lastName: 'Spencer', age: 36 }
-];
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -26,7 +19,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data);
       }
     }
   }
