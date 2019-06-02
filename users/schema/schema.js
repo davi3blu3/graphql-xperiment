374db95f3545cbf806d1d24ba838566fc5bb82d2
Dave Harned <dharned@gmail.com>
2019-06-01 21:28:43 -0500

    build out schema and root query, add schema to server

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -9,4 +11,27 @@ const UserType = new GraphQLObjectType({
     lastName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const users = [
+  { id: '23', firstName: 'Bill', lastName: 'Rogers', age: 20 },
+  { id: '47', firstName: 'Samantha', lastName: 'Green', age: 22 },
+  { id: '51', firstName: 'Susan', lastName: 'Spencer', age: 36 }
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
